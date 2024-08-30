48bf4745a33be9a0adf1854bddc35dc05b95ed50
=JGPark81 <=wow4man@gmail.com>
2024-08-30 17:41:05 +0900

    first 0830

---
+++ auto.sql
@@ -0,0 +1,15 @@
+
+declare @name nvarchar(max)
+   , @path nvarchar(max)
+   , @prefix nvarchar(max)
+   , @driverpath nvarchar(max)
+   , @beforeweek nvarchar(30)
+   , @extension nvarchar(10)
+
+set @driverpath ='d:\backupgit\'
+set @prefix ='doorien'
+set @extension ='bak'
+set @path =@driverpath + @prefix + '.' + @extension
+
+BACKUP DATABASE [doorien] TO DISK =@path WITH INIT , STATS=10
+
